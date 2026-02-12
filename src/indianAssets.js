130b1904b7fe182260ac4b7c5c5a0a5eb234e3cd
satyapriyaarya <satyapriyaarya@gmail.com>
2026-02-12 16:20:24 +0530

    Replace site with coming soon page

--- src/indianAssets.js
+++
@@ -1,9 +0,0 @@
-// Indian themed SVG background patterns and icons
-export const indianPatterns = {
-  paisley: `<svg width="120" height="120" viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="60" cy="60" rx="50" ry="30" fill="#ff9933" fill-opacity="0.12"/><ellipse cx="60" cy="60" rx="30" ry="15" fill="#138808" fill-opacity="0.10"/><ellipse cx="60" cy="60" rx="12" ry="6" fill="#000080" fill-opacity="0.12"/></svg>`,
-  lotus: `<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M40 70C55 60 70 45 40 10C10 45 25 60 40 70Z" fill="#ff9933" fill-opacity="0.13"/><ellipse cx="40" cy="55" rx="10" ry="5" fill="#138808" fill-opacity="0.10"/></svg>`
-};
-
-export const indianIcons = {
-  diya: `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="16" cy="24" rx="12" ry="6" fill="#ff9933"/><ellipse cx="16" cy="24" rx="8" ry="3" fill="#fff" fill-opacity="0.7"/><path d="M16 8C18 12 14 14 16 18C18 14 14 12 16 8Z" fill="#ffd700"/></svg>`
-};
